d9fc82ab574aedd918707627364fdd8d2714e4bd
laillavcortes <laillavcortes@hotmail.com>
2021-04-27 01:27:50 +0100

    Criando classes de Aluno

---
+++ src/main/java/com/naica/repositories/AlunoRepository.java
@@ -0,0 +1,11 @@
+package com.naica.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.naica.domain.Aluno;
+
+public interface AlunoRepository extends JpaRepository<Aluno,Integer>{
+
+	
+	
+}
